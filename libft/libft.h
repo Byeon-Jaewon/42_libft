5892ddb2c30b4818b768049d14aa0a57cc884273
Byeon-Jaewon <bjw5095@gmail.com>
2020-12-29 23:44:15 +0900

    part2

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jbyeon <jbyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 18:46:07 by jbyeon            #+#    #+#             */
-/*   Updated: 2020/12/27 16:03:10 by jbyeon           ###   ########.fr       */
+/*   Updated: 2020/12/29 23:04:10 by jbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,7 @@ void		*ft_calloc(size_t nmemb, size_t size);
 void		ft_bzero(void *s, size_t n);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
 #endif
