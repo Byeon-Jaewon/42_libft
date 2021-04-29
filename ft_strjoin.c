bb9e6c93978bd1859414a2594f67ce0b0b091676
jbyeon <jbyeon@student.42seoul.kr>
2021-04-29 11:56:58 +0900

    modify

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jbyeon <jbyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 15:53:55 by jbyeon            #+#    #+#             */
-/*   Updated: 2021/02/08 22:19:58 by jbyeon           ###   ########.fr       */
+/*   Updated: 2020/12/27 16:32:22 by jbyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,29 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	char	*tmp1;
-	char	*tmp2;
-	size_t	size;
+	char	*p;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+	size_t	j;
 
-	if (s1 == NULL || s2 == NULL)
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (!(p = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))))
 		return (NULL);
-	tmp1 = (char *)s1;
-	tmp2 = (char *)s2;
-	size = ft_strlen(tmp1) + ft_strlen(tmp2);
-	if (!(str = (char *)malloc(size + 1)))
-		return (NULL);
-	str[0] = '\0';
-	ft_strlcat(str, tmp1, ft_strlen(tmp1) + 1);
-	ft_strlcat(str, tmp2, ft_strlen(str) + ft_strlen(tmp2) + 1);
-	return (str);
+	i = 0;
+	while (i < s1_len)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < s1_len + s2_len)
+	{
+		p[i] = s2[j];
+		i++;
+		j++;
+	}
+	p[i] = '\0';
+	return (p);
 }
